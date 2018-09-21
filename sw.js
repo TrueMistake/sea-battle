2c1085e191900b59b684f0768fb6a75f21d0ec55
TrueMistake <ooghon@gmail.com>
2018-09-21 21:20:59 +0300

    push message

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 ‘use strict’;
 importScripts(‘sw - toolbox.js’);
+self.addEventListener('push', function(event) {
+    event.waitUntil(
+        self.registration.showNotification('Got Push?', {
+            body: 'Push Message received'
+        }));
+});
 toolbox.precache([“index.html”]);
 toolbox.router.get(‘/ *’,toolbox.cacheFirst);
 toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
