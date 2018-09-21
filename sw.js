1853f230d033a46a0f6fc21cdd2be90d65800a5f
TrueMistake <ooghon@gmail.com>
2018-09-21 21:38:11 +0300

    hfghf

--- sw.js
+++ sw.js
@@ -10,4 +10,27 @@ self.addEventListener('push', function(event) {
             body: 'Push Message received'
         }));
 });
-
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('the-magic-cache').then(function(cache) {
+        });
+);
+});
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('the-magic-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
