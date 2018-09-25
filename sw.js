2cf4c64c968105882814cdd886073d68aa244f95
TrueMistake <ooghon@gmail.com>
2018-09-25 17:13:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 importScripts('sw-toolbox.js');
 
-toolbox.precache([“index.html”]);
+//toolbox.precache([“index.html”]);
 toolbox.router.get('/ *',toolbox.cacheFirst);
 toolbox.router.get('/ *', toolbox.networkFirst, { networkTimeoutSeconds: 5});
 /*‘use strict’;
